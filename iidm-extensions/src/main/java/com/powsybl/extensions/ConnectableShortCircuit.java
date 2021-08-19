4eb3cf5209c945f929775e1d3aaba457214de846
Coline Piloquet <coline.piloquet@rte-france.com>
2021-08-19 17:22:14 +0200

    Add connectable short circuit implementation and interface
    Signed-off-by: Coline Piloquet <coline.piloquet@rte-france.com>

---
+++ iidm-extensions/src/main/java/com/powsybl/extensions/ConnectableShortCircuit.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.extensions;
+
+import com.powsybl.commons.extensions.AbstractExtension;
+import com.powsybl.commons.extensions.Extension;
+import com.powsybl.iidm.network.Connectable;
+
+/**
+ * @author Coline Piloquet <coline.piloquet@rte-france.com>
+ */
+public interface ConnectableShortCircuit extends Extension<Connectable> {
+    @Override
+    default String getName() {
+        return "busBarSectionShortCircuit";
+    }
+
+    public double getIpMax();
+
+    public void setIpMax(double ipMax);
+
+    public double getIpMin();
+
+    public void setIpMin(double ipMin);
+}
---
+++ iidm-extensions/src/main/java/com/powsybl/extensions/ConnectableShortCircuitImpl.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.extensions;
+
+import com.powsybl.commons.extensions.AbstractExtension;
+import com.powsybl.iidm.network.Connectable;
+
+/**
+ * @author Coline Piloquet <coline.piloquet@rte-france.com>
+ */
+public class ConnectableShortCircuitImpl extends AbstractExtension<Connectable> implements ConnectableShortCircuit {
+
+    private double ipMax; // maximum allowable peak short-circuit current of busbar
+    private double ipMin; // minimum allowable peak short-circuit current of busbar
+
+    public ConnectableShortCircuitImpl(double ipMax, double ipMin) {
+        this.ipMax = ipMax;
+        this.ipMin = ipMin;
+    }
+
+    @Override
+    public double getIpMax() {
+        return ipMax;
+    }
+
+    @Override
+    public void setIpMax(double ipMax) {
+        this.ipMax = ipMax;
+    }
+
+    @Override
+    public double getIpMin() {
+        return ipMin;
+    }
+
+    @Override
+    public void setIpMin(double ipMin) {
+        this.ipMin = ipMin;
+    }
+}
