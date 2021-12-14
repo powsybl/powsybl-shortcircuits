19ac2ac3012eee85ed08e3a03b0326b8d214214b
José Antonio Marqués <marquesja@aia.es>
2021-12-14 13:05:16 +0100

    Rename package
    Signed-off-by: José Antonio Marqués <marquesja@aia.es>

--- shortcircuits-converters/src/test/java/com/powsybl/shortcircuits/converters/ShortCircuitCgmesConformity1ModifiedCatalog.java
+++ shortcircuits-converters/src/test/java/com/powsybl/shortcircuits/converters/ShortCircuitCgmesConformity1ModifiedCatalog.java
@@ -5,7 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-package com.powsybl.shortcircuits.importers;
+package com.powsybl.shortcircuits.converters;
 
 import com.powsybl.cgmes.model.test.TestGridModelResources;
 import com.powsybl.commons.datasource.ResourceSet;
