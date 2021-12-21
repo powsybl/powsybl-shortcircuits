8ed5b4058bf6e7634eade436fd8efe01161a3506
Coline Piloquet <coline.piloquet@rte-france.com>
2021-12-21 11:43:09 +0100

    Add test from config file
    Signed-off-by: Coline PILOQUET <coline.piloquet@rte-france.com>

--- shortcircuits-api/src/test/java/com/powsybl/shortcircuits/ShortCircuitParametersTest.java
+++ shortcircuits-api/src/test/java/com/powsybl/shortcircuits/ShortCircuitParametersTest.java
@@ -11,12 +11,16 @@ import com.google.common.jimfs.Configuration;
 import com.google.common.jimfs.Jimfs;
 import com.powsybl.commons.config.InMemoryPlatformConfig;
 import com.powsybl.commons.config.PlatformConfig;
+import com.powsybl.commons.config.YamlModuleConfigRepository;
 import com.powsybl.commons.extensions.AbstractExtension;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static org.junit.Assert.*;
 
@@ -77,6 +81,17 @@ public class ShortCircuitParametersTest {
         assertFalse(parameters.isSubTransStudy());
 
         parameters.setSubTransStudy(true);
+        assertTrue(parameters.isSubTransStudy());
+    }
+
+    @Test
+    public void testConfigLoader() throws IOException {
+        Path cfgDir = Files.createDirectory(fileSystem.getPath("config"));
+        Path cfgFile = cfgDir.resolve("config.yml");
+
+        Files.copy(getClass().getResourceAsStream("/config.yml"), cfgFile);
+        PlatformConfig platformConfig = new PlatformConfig(new YamlModuleConfigRepository(cfgFile), cfgDir);
+        ShortCircuitParameters parameters = ShortCircuitParameters.load(platformConfig);
         assertTrue(parameters.isSubTransStudy());
     }
 
